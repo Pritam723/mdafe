34de2d5406bd46a95daafbd85b51cec55121f00d
PRITAM DUTTA <00091@erldc.net>
2025-04-30 08:24:37 +0530

    30th April 2025

--- src/dynamicFiles/dataFetching/Home.js
+++ src/dynamicFiles/dataFetching/Home.js
@@ -5,3 +5,37 @@ const Home = () => {
 };
 
 export default Home;
+
+// import React from "react";
+// import { useSpring, animated } from "react-spring";
+// import meterImage from "../../GI-Nav.png";
+
+// const Home = () => {
+//   const springProps = useSpring({
+//     from: { opacity: 0, marginTop: -50 },
+//     to: { opacity: 1, marginTop: 0 },
+//     config: { duration: 1000 },
+//   });
+
+//   return (
+//     <div className="home-container">
+//       <animated.div style={springProps}>
+//         <h1>Welcome to the Meter Data Archival Web Application</h1>
+//       </animated.div>
+//       <animated.div style={springProps}>
+//         <p>
+//           This application allows you to manage and archive meter data
+//           efficiently.
+//         </p>
+//       </animated.div>
+//       <animated.img
+//         src={meterImage}
+//         alt="Meter Data"
+//         style={springProps}
+//         className="meter-image"
+//       />
+//     </div>
+//   );
+// };
+
+// export default Home;
